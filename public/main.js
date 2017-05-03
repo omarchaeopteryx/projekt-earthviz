f9423891c5087f9645915196b663a42e7c566d1d
omarchaeopteryx <omaralimalik@gmail.com>
2017-05-03 15:39:58 -0700

    Add rudimentary color changes upon keystroke

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
-  console.log('hello world!');
+  console.log('hello, earth!');
+
+  let world = $("#world");
+  let sea = $("#message");
 
   $(document).on('keypress', function(e){
         console.log(e.which);
@@ -15,27 +18,35 @@ $( document ).ready(function() {
                 break;
             case 97:
                 console.log("a");
+                world.css("stroke","blue");
                 break;
             case 115:
                 console.log("s");
+                world.css("stroke","green");
                 break;
             case 100:
                 console.log("d");
+                world.css("stroke","purple");
                 break;
             case 102:
                 console.log("f");
+                world.css("stroke","red");
                 break;
             case 106:
                 console.log("j");
+                sea.css("background-color","lightBlue");
                 break;
             case 107:
                 console.log("k");
+                sea.css("background-color","lightGrey");
                 break;
             case 108:
                 console.log("l");
+                sea.css("background-color","pink");
                 break;
             case 59:
                 console.log(";");
+                sea.css("background-color","maroon");
                 break;
         }
     });
